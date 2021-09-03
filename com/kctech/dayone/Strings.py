57eb28d002699ba1f339b35c3f37f000a27fb353
saravanan19999 <git config --global user.email saravanan_eakambaram@kctechservices.com>
2021-09-03 12:41:07 +0530

    hi

---
+++ com/kctech/dayone/Strings.py
@@ -0,0 +1,34 @@
+#variable strings and numbers
+#basics in strings
+
+#1.---uppercase,title,lower-----
+
+name1="python"
+#name1=input("Enter Your Name ")
+print(name1.upper())
+print(name1.title())
+print(name1.lower())
+
+#2.Concatenation
+first="saravanan"
+last="adithiya"
+fullname= first+" "+last
+print(fullname)
+
+#3.sentences
+first="swami"
+last="vivekanda:"
+fullname= first+" "+last
+sentences= fullname+" " + \
+    "You cannot believe in God until you believe in yourself."
+print(sentences)
+
+#4.strip
+
+name="  test  "
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+
+
