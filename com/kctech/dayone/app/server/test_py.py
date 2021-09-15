5c70a3737393274e5fac098adc958c4249c00c0b
saravanan19999 <git config --global user.email saravanan_eakambaram@kctechservices.com>
2021-09-15 10:26:12 +0530

    gcit

--- com/kctech/dayone/app/server/test_py.py
+++ com/kctech/dayone/app/server/test_py.py
@@ -1,17 +1,18 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from app.server.app import app
+from app.server.route.student import router
 
-app = FastAPI()
+client=TestClient(router)
 
+data ={
+          "fullname": "John Doe",
+          "email": "jdoe@x.edu.ng",
+          "course_of_study": "Water resources engineering",
+          "year": 2,
+          "gpa": "3.0",
 
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
+}
 
-
-client = TestClient(app)
-
-
-def test_read_main():
-    response = client.get("/")
+def test_insert():
+    response = client.post("/",json=data)
     assert response.status_code == 200
